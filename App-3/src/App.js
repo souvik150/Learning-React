49b3feeb0f1cbb839e84a6db423f222fbbb43619
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-04-07 01:49:44 +0530

    Done

--- App-3/src/App.js
+++ App-3/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
       updatedGoals.unshift({ text: enteredText, id: "goal1" });
+      // updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
